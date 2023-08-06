dd99f9982652106ea1d9b9b7b5ac46dec70257e6
Simon Tamang <gomjasimon555@gmail.com>
2023-08-06 08:26:21 +0545

    Header is done

---
+++ src/components/Header.js
@@ -0,0 +1,103 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+<UserImg src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3f01db52-f675-48dc-9c91-f245d99f1486/d2nqynw-af694fd2-e1ba-4e9c-badb-630a48474599.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNmMDFkYjUyLWY2NzUtNDhkYy05YzkxLWYyNDVkOTlmMTQ4NlwvZDJucXludy1hZjY5NGZkMi1lMWJhLTRlOWMtYmFkYi02MzBhNDg0NzQ1OTkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.59_LN0TnrsDrVLS266jLpfZZfte_OZeNGkNQFJzgQCM"></UserImg>
+
+
+    </Nav>
+  );
+};
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex:1;
+  margin-left:25px;
+  align-items:center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor:pointer;
+
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position:relative;
+
+      &:after{
+        content:"";
+        height:2px;
+        background:white;
+        position:absolute;
+        left:0;
+        right:0;
+        bottom:-6px;
+        opacity:0;
+        transform-origin: top center;
+        transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+        transform:scaleX(0);
+      }
+    }
+    &:hover{
+      span:after{
+        transform:scaleX(1);
+        opacity:1;
+      }
+    }
+  }
+`;
+
+const UserImg = styled.img`
+width:48px;
+height:48px;
+border-radius:50%;
+cursor:pointer;
+`
